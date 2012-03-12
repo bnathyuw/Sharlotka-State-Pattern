e020233fe9cc08110998a23f1a63c873da0ab4aa
Matthew Butt <matthew.butt@7digital.com>
2012-03-12 15:45:24 +0000

    Wrote breaking acceptance tests

---
+++ Classic.Acceptance.Tests/Tests.cs
@@ -0,0 +1,24 @@
+﻿using Classic.Implementation;
+using NUnit.Framework;
+
+namespace Classic.Acceptance.Tests
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void Happy_path() {
+			var sharlotka = new Sharlotka();
+			sharlotka.AddApples();
+			sharlotka.AddBatter();
+			sharlotka.Bake();
+			while (!sharlotka.IsReady) {
+				sharlotka.Bake();
+			}
+			sharlotka.TurnOut();
+			sharlotka.DustWithSugar();
+			sharlotka.DustWithCinnamon();
+			sharlotka.Serve();
+		}
+	}
+}
---
+++ Classic.Implementation/Sharlotka.cs
@@ -0,0 +1,37 @@
+﻿namespace Classic.Implementation
+{
+	public class Sharlotka
+	{
+		public void AddApples() {
+			throw new System.NotImplementedException();
+		}
+
+		public void AddBatter() {
+			throw new System.NotImplementedException();
+		}
+
+		public void Bake() {
+			throw new System.NotImplementedException();
+		}
+
+		public bool IsReady {
+			get { throw new System.NotImplementedException(); }
+		}
+
+		public void TurnOut() {
+			throw new System.NotImplementedException();
+		}
+
+		public void DustWithSugar() {
+			throw new System.NotImplementedException();
+		}
+
+		public void DustWithCinnamon() {
+			throw new System.NotImplementedException();
+		}
+
+		public void Serve() {
+			throw new System.NotImplementedException();
+		}
+	}
+}
