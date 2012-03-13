5f73e3cc9fe291c6c6d9e0fa352d2ae01e3ef029
Matthew Butt <matthew.butt@7digital.com>
2012-03-13 10:29:11 +0000

    Small Interface Integration Tests

---
+++ SmallInterface.Integration.Tests/StructureMapTests.cs
@@ -0,0 +1,15 @@
+﻿using SmallInterface.Implementation;
+using NUnit.Framework;
+
+namespace SmallInterface.Integration.Tests
+{
+	[TestFixture]
+	public class StructureMapTests
+	{
+		[Test]
+		public void Configuration_is_valid() {
+			var container = DependencyResolver.Container;
+			container.AssertConfigurationIsValid();
+		}
+	}
+}
